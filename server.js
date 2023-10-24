b2869d4166b968d5c848f4f8eaf235cd4d022c74
Maks <Crossfade143@gmail.com>
2023-10-24 13:53:12 +0300

    server is done

---
+++ server.js
@@ -0,0 +1,24 @@
+const { log } = require("console");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 4000;
+
+app.get("/get-json", (req, res) => {
+  const jsonFilePath = "./data/podcast-list.json";
+
+  fs.readFile(jsonFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Помилка читання JSON-файлу:", err);
+      res.status((500).json({ error: "Помилка сервера" }));
+    } else {
+      const jsonData = JSON.parse(data);
+
+      res.json(jsonData);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Сервер працює на порті ${port}`);
+});
